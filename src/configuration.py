8310e51b45adab6ff81b77d118d0ee9d0acf0f34
AnnaHoian <anna.fialk@gmail.com>
2025-10-23 19:36:44 +0300

    DEMO-QA-004 Added pytest
    Moved configuration module to the src
    Adjust config ini for pytest

--- src/configuration.py
+++ src/configuration.py
@@ -1,14 +1,20 @@
 import configparser
 import os
+from pathlib import Path
 
 
 class Configuration:
 
     def __init__(self):
         self.parser = configparser.ConfigParser()
-        self.parser.read(os.getenv('CONFIG_PATH', 'config.ini'))
+        self.parser.read(Path(os.getenv('CONFIG_PATH', 'config.ini')).absolute())
 
     def get_browser(self):
         """ Get Selenium instance of driver (Chrome, FF, etc.)"""
         # TODO DEMO-QA-003 Implement getting selenium driver in configuration class
         pass
+
+    def get_base_url(self):
+        return self.parser.get('demoqa', 'url')
+
+CONFIG = Configuration()
