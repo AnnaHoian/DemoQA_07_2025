f0d0ed079810158237137b37bacd79f8638fe329
AnnaHoian <anna.fialk@gmail.com>
2025-10-23 20:44:41 +0300

    DEMO-QA-005 Add BasePage for web elements and web app

--- configuration.py
+++ configuration.py
@@ -1,14 +1,23 @@
 import configparser
 import os
+from pathlib import Path
 
 
 class Configuration:
 
     def __init__(self):
-        self.parser = configparser.ConfigParser()
-        self.parser.read(os.getenv('CONFIG_PATH', 'config.ini'))
+        self._parser = configparser.ConfigParser()
+        self._parser.read(Path(os.getenv("CONFIG_PATH", "config.ini")).absolute())
+
 
     def get_browser(self):
-        """ Get Selenium instance of driver (Chrome, FF, etc.)"""
-        # TODO DEMO-QA-003 Implement getting selenium driver in configuration class
+        """Get selenium instance of driver (chrome, firefox etc.)"""
+        # TODO DEMO-QA-003 Implement getting selenium driver in config class
         pass
+
+    def get_base_url(self):
+        return self._parser['demoqa'].get('url')
+
+
+
+CONFIG = Configuration()
